c48db872208d5d0b136066f93a871053089c0d8d
Antoine Larochelle <royal@multitel-stage.multitel.com>
2024-10-24 15:39:08 -0400

    Refactoring and adding validations on username, password and role

--- Models/User.cs
+++ Models/User.cs
@@ -1,5 +1,8 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson;
+using System;
+using System.Text.RegularExpressions;
+using Automate.Utils.Constants;
 
 namespace Automate.Models
 {
@@ -9,12 +12,62 @@ namespace Automate.Models
         public ObjectId Id { get; set; }
 
         [BsonElement("Username")]
-        public string? Username { get; set; }
+        public string Username { 
+            get
+            {
+                return Username;
+            } 
+            set 
+            {
+                ArgumentNullException.ThrowIfNull(value);
+                if (value.Length > 0) 
+                {
+                    Username = value;   
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
 
         [BsonElement("Password")]
-        public string? Password { get; set; }
+        public string Password {
+            get
+            {
+                return Password;
+            }
+            set
+            {
+                ArgumentNullException.ThrowIfNull(value);
+                if (!Regex.IsMatch(Password, "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?.&])[A-Za-z\\d@$!%*?.&]{8,}$/"))
+                {
+                    throw new ArgumentException();
+                }
+                if (value.Length <= 0)
+                {
+                    throw new ArgumentException();
+                }
+                Password = value;
+            }
+        }
 
         [BsonElement("Role")]
-        public string? Role { get; set; }
+        public string Role {
+            get
+            {
+                return Role;
+            }
+            set
+            {
+                
+                ArgumentNullException.ThrowIfNull(value);
+                if (value != RoleConstant.ADMIN && value != RoleConstant.EMPLOYEES)
+                {
+                    throw new ArgumentException();
+                }
+                Role = value;
+            } 
+        }
     }
 }
---
+++ Utils/Constants/RoleConstant.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automate.Utils.Constants
+{
+    public class RoleConstant
+    {
+        public const string EMPLOYEES = "employe";
+        public const string ADMIN = "admin";
+    }
+}
