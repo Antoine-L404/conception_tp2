6238911c5ff9b2910360db798e54e9ef16c19333
Thibault Castelli <thcas004@edu.cegepgarneau.ca>
2024-10-25 10:41:14 -0400

    Refactor login view

--- Automate/Views/LoginWindow.xaml.cs
+++ Automate/Views/LoginWindow.xaml.cs
@@ -1,19 +1,6 @@
 ﻿using Automate.ViewModels;
-using Automate.Utils;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Automate
 {
@@ -34,7 +21,7 @@ namespace Automate
             PasswordBox? passwordBox = sender as PasswordBox;
             if (DataContext is LoginViewModel viewModel)
             {
-                viewModel.Password = passwordBox.Password; // Met à jour la propriété Password dans le ViewModel
+                viewModel.Password = passwordBox?.Password;
             }
         }
     }
