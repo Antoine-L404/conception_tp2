6794ac93abe1caf3e7b1ccb609c5c372241ba257
pierreolivierc <115761312+pierreolivierc@users.noreply.github.com>
2024-10-29 13:24:41 -0400

    calendrier fonctionnel

---
+++ Automate/Views/CalendarWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Automate.Views
+{
+    /// <summary>
+    /// Interaction logic for CalendarWindow.xaml
+    /// </summary>
+    public partial class CalendarWindow : Window
+    {
+        public CalendarWindow()
+        {
+            InitializeComponent();
+
+            myCalendar.DisplayDate = DateTime.Today;
+        }
+
+
+    }
+}
