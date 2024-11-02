e74118cf72e37bd4810d6fcee55ec582315f3626
pierreolivierc <115761312+pierreolivierc@users.noreply.github.com>
2024-11-02 15:14:50 -0400

    task model

---
+++ Automate/Models/Task.cs
@@ -0,0 +1,40 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace Automate.Models
+{
+    class Task
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        private string _title = "";
+        [BsonElement("Title")]
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                ArgumentNullException.ThrowIfNull(value);
+                if (value.Trim().Length == 0)
+                {
+                    throw new ArgumentException();
+                }
+                _title = value;
+            }
+        }
+
+        private DateTime _eventDate;
+        [BsonElement("DateEvenement")]
+        public DateTime EventDate
+        {
+            get => _eventDate;
+            set
+            {
+                _eventDate = value; 
+            }
+        }
+
+    }
+}
