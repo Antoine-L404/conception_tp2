f0d1c841bf1733c62616659167d57cdda20f666f
pierreolivierc <115761312+pierreolivierc@users.noreply.github.com>
2024-11-02 19:25:20 -0400

    avancement d'ajout d'un évenement

--- Automate/Views/CalendarWindow.xaml.cs
+++ Automate/Views/CalendarWindow.xaml.cs
@@ -7,7 +7,7 @@ namespace Automate.Views
     public partial class CalendarWindow : Window
     {
         private readonly CalendarCommand _calendarCommand;
-
+        private DateTime? selectedDate;
         public CalendarWindow()
         {
             InitializeComponent();
@@ -26,7 +26,46 @@ namespace Automate.Views
 
         private void OnCalendarDateClicked(object sender, MouseButtonEventArgs e)
         {
-            _calendarCommand.Execute(e);
+            if (myCalendar.SelectedDate.HasValue)
+            {
+                selectedDate = myCalendar.SelectedDate.Value;
+            }
+        }
+
+        private void OnAddEventClick(object sender, RoutedEventArgs e)
+        {
+            if (selectedDate.HasValue)
+            {
+                TaskFormWindow eventForm = new TaskFormWindow(selectedDate.Value);
+                eventForm.ShowDialog();
+
+                if (eventForm.IsConfirmed)
+                {
+                    MessageBox.Show($"Événement '{eventForm.SelectedEventType}' ajouté pour le {eventForm.EventDate.ToShortDateString()}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Veuillez sélectionner une date dans le calendrier.", "Erreur", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void OnEditEventClick(object sender, RoutedEventArgs e)
+        {
+            if (selectedDate.HasValue)
+            {
+                TaskFormWindow eventForm = new TaskFormWindow(selectedDate.Value);
+                eventForm.ShowDialog();
+
+                if (eventForm.IsConfirmed)
+                {
+                    MessageBox.Show($"Événement '{eventForm.SelectedEventType}' modifié pour le {eventForm.EventDate.ToShortDateString()}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Veuillez sélectionner une date dans le calendrier.", "Erreur", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
         }
     }
 }
---
+++ Automate/Views/TaskFormWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Automate.Views
+{
+    /// <summary>
+    /// Interaction logic for TaskFormWindow.xaml
+    /// </summary>
+    public partial class TaskFormWindow : Window
+    {
+        public string SelectedEventType { get; private set; }
+        public DateTime EventDate { get; private set; }
+        public bool IsConfirmed { get; private set; }
+
+        public TaskFormWindow(DateTime selectedDate)
+        {
+            InitializeComponent();
+            EventDate = selectedDate;
+            eventDateTextBox.Text = EventDate.ToShortDateString();
+        }
+
+        private void OnAddButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (eventTypeComboBox.SelectedItem is ComboBoxItem selectedItem)
+            {
+                SelectedEventType = selectedItem.Content.ToString();
+                IsConfirmed = true;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Veuillez sélectionner un type d'événement.", "Erreur", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void OnCancelButtonClick(object sender, RoutedEventArgs e)
+        {
+            IsConfirmed = false;
+            this.Close();
+        }
+    }
+}
